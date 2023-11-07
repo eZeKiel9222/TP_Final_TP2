8722d1c4a022241325d59d49b5838bd034befda2
mlarotonda <mauricio.larotonda@fravega.com.ar>
2023-11-06 23:10:49 -0300

    Se agrega cors

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ import express from "express"
 import router from "./routes/router.js";
 import connection from "./connection/connection.js";
 import { config } from "./config/config.js";
+import cors from "cors"
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }))
 app.use("/api", router);
